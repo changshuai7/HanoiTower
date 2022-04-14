d1deecc98a9d265f2b2633a4a5ebf8bc069af944
changshuai <changshuai@renrenche.com>
2022-04-14 16:16:44 +0800

    1、添加游戏规则入口

--- src/hanoi_gui/HanoiWindow.java
+++ src/hanoi_gui/HanoiWindow.java
@@ -13,8 +13,9 @@ public class HanoiWindow extends JFrame implements ActionListener {
     int amountOfDisc = 2;
     char[] towerName = {'A', 'B', 'C'};
     JMenuBar bar;
-    JMenu menuGrade;
+    JMenu menuGrade,menuRule;
     JMenuItem oneGradeItem, twoGradeItem, threeGradeItem;
+    JMenuItem gameRuleItem;
     JButton renew = null;
     JButton autoButton = null;
     JPanel center = new JPanel();
@@ -29,7 +30,9 @@ public class HanoiWindow extends JFrame implements ActionListener {
 
         add(tower, BorderLayout.CENTER);
         bar = new JMenuBar();
-        menuGrade = new JMenu("选择级别");
+
+        // 难度级别
+        menuGrade = new JMenu("难度级别");
         oneGradeItem = new JMenuItem("初级");
         twoGradeItem = new JMenuItem("中级");
         threeGradeItem = new JMenuItem("高级");
@@ -39,10 +42,19 @@ public class HanoiWindow extends JFrame implements ActionListener {
         menuGrade.add(threeGradeItem);
 
         bar.add(menuGrade);
+
+        // 游戏规则
+        menuRule = new JMenu("游戏规则");
+        gameRuleItem = new JMenuItem("游戏规则");
+        menuRule.add(gameRuleItem);
+
+        bar.add(menuRule);
+
         setJMenuBar(bar);
         oneGradeItem.addActionListener(this);
         twoGradeItem.addActionListener(this);
         threeGradeItem.addActionListener(this);
+        gameRuleItem.addActionListener(this);
 
         renew = new JButton("重新开始");
         renew.addActionListener(this);
@@ -93,6 +105,12 @@ public class HanoiWindow extends JFrame implements ActionListener {
             tower.getAutoMoveDisc().setLocation(x, y);
             tower.getAutoMoveDisc().setSize(280, this.getBounds().height);
             tower.getAutoMoveDisc().setVisible(true);
+        }else if (e.getSource() == gameRuleItem){
+            JOptionPane.showMessageDialog(null, "从左到右有A、B、C三根柱子\n" +
+                    "其中A柱子上面有从小叠到大的若干个圆盘\n" +
+                    "现要求将A柱子上的圆盘移到C柱子上去\n" +
+                    "期间只有一个原则：一次只能移到一个盘子且大盘子不能在小盘子上面",null, JOptionPane.INFORMATION_MESSAGE);
+
         }
         validate();
     }
